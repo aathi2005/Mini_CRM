ccb904aa8b3c5f7a3361df7b1e9548ffee190081
aathi2005 <aathirithikas@gmail.com>
2026-04-29 15:28:30 +0530

    feat: add Netlify deployment config and update CORS for production

--- server/server.js
+++ server/server.js
@@ -9,7 +9,11 @@ connectDB();
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+const allowedOrigins = [
+  'http://localhost:5173',
+  process.env.FRONTEND_URL,
+].filter(Boolean);
+app.use(cors({ origin: allowedOrigins, credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
